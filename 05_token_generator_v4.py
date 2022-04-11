4743034bf9551a9f18b5902fad448efed69741ec
Taylorjonty <98923451+Taylorjonty@users.noreply.github.com>
2022-04-12 09:23:50 +1200

    codes

--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -8,15 +8,18 @@ import random
 starting_balance = 100
 balance = starting_balance
 
-# Testing loop to generate 100 tokens
+# Testing loop to generate 10 tokens
 for item in range(10):
     number = random.randint(1, 100)
 
-    # adjust balance
+    # If the random number generated is
+    # lower than 5, it will show unicorn
     if number <= 5:
         token = "unicorn"
         balance += 4
-    elif number <= 36:
+    # If the random number is lower or equal
+    # to 35, it will show donkey
+    elif number <= 35:
         token = "donkey"
         balance -= 1
     else:
@@ -30,6 +33,7 @@ for item in range(10):
 # output
     print(f"Token: {token}. Balance: ${balance:.2f}")
 
+# showing final balance at the end
 print()
 print(f"Starting balance = ${starting_balance:.2f}")
 print(f"final balance = ${balance:.2f}")
